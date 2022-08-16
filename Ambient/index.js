1fbac1bc66b82256a468c3207fe5756a10135c65
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-16 13:36:53 -0300

    Tipagem de variáveis

---
+++ Ambient/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const personName = 'Daniel';
+const personAge = 23;
+const personaLIVE = true;
+const frutas = ['banana', 'morango'];
+const ano = [1998, 2025];
+const valores = ['jefferson', 1998];
