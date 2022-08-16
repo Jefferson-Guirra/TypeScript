86ca5dd8cf32214638665fa58c9c616a4a63ab6c
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-16 13:47:01 -0300

    Type aliasing.

--- Ambient/index.ts
+++
@@ -1,6 +0,0 @@
-const personName:string = 'Daniel'
-const personAge:number = 23
-const personaLIVE:Boolean = true
-const frutas :string[] = ['banana','morango']
-const ano : number[] = [1998,2025]
-const valores : (number | string) [] = ['jefferson',1998]
---
+++ Aula-02/Type aliasing/index.ts
@@ -0,0 +1,7 @@
+type idade = number | string
+
+type Sex = 'male' | 'female' | undefined
+
+const age : idade = '25'
+
+const sex : Sex = undefined
