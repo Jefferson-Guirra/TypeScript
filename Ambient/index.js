6fb10b2c3fd9623c51b94e5b0dc920392e7118a7
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-18 20:23:01 -0300

    statics

--- Ambient/index.js
+++ Ambient/index.js
@@ -1,16 +1,13 @@
 "use strict";
 class Pessoa {
-    constructor(nome, idade, sobrenome, cpf) {
-        this.idade = idade;
+    constructor(nome, sobrenome, idade, _cpf) {
         this.nome = nome;
         this.sobrenome = sobrenome;
-        this._cpf = cpf;
+        this.idade = idade;
+        this._cpf = _cpf;
     }
-    getIdade() {
-        return this.idade;
-    }
-    getNomeCompleto() {
-        return `${this.nome} ${this.sobrenome}`;
+    static criaPessoa(nome, sobrenome) {
+        return new Pessoa(nome, sobrenome, Pessoa.idadePadrao, Pessoa.cpfPadrao);
     }
     set cpf(cpf) {
         this._cpf = cpf;
@@ -19,6 +16,11 @@ class Pessoa {
         return this._cpf.replace(/\D/g, '');
     }
 }
-const pessoa = new Pessoa('jefferson', 23, 'guirra', '076.444.445-05');
+Pessoa.idadePadrao = 0;
+Pessoa.cpfPadrao = '000.000.000-00';
+const pessoa1 = Pessoa.criaPessoa('jefferson', 'guirra');
+const pessoa = new Pessoa('jefferson', 'guirra', 23, '07644444505');
 pessoa.cpf = '076.444.445-05';
-console.log(pessoa.cpf);
+console.log(pessoa1);
+console.log(pessoa);
+//ATRIBUTOS E METODOS ESTATICOS PODEM SER ACESSADOS DIRETAMENTE NO CONSTRUTOR PRINCIPAL (PESSOA)
