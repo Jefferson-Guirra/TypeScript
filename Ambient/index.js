333c3d855d56fa5b61849004ea0ef46940fba8df
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-18 16:05:12 -0300

    Classes

--- Ambient/index.js
+++ Ambient/index.js
@@ -1,5 +1,23 @@
 "use strict";
-const verifyUser = (user, sentUser) => {
-    return user.password === sentUser.password && user.user === sentUser.user;
-};
-console.log(verifyUser({ password: 123, user: 'jefferson' }, { password: 123, user: 'jefferson' }));
+class Empresa {
+    constructor(nome) {
+        this.colaboradores = [];
+        this.nome = nome;
+    }
+    adicionaColaboradores(colaborador) {
+        this.colaboradores.push(colaborador);
+    }
+}
+class Colaborador {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
+const colaborador1 = new Colaborador('Jefferson', 'Guirra');
+const colaborador2 = new Colaborador('Mirela', 'Tamires');
+const empresa1 = new Empresa('Jefferson');
+empresa1.adicionaColaboradores(colaborador1);
+empresa1.adicionaColaboradores(colaborador2);
+console.log(empresa1);
+console.log(empresa1.nome);
