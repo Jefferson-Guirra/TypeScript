93e6eda3877311affcc35022c2b5c615ad68d7f6
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-20 13:42:33 -0300

    metodos e atributos abstratos

--- Ambient/index.js
+++ Ambient/index.js
@@ -1,17 +1,31 @@
 "use strict";
-class Database {
-    constructor(host, user, password) {
-        this.host = host;
-        this.user = user;
-        this.password = password;
+class Personagem {
+    constructor(nome, ataque, vida) {
+        this.nome = nome;
+        this.ataque = ataque;
+        this.vida = vida;
     }
-    static getDatabase(host, user, password) {
-        if (Database.database)
-            return Database.database;
-        Database.database = new Database(host, user, password);
-        return Database.database;
+    perderVida(ataque) {
+        this.vida -= ataque;
+        console.log(`${this.nome} agora tem ${this.vida} pontos de vida.`);
+    }
+    atacar(person) {
+        this.bordao();
+        person.perderVida(this.ataque);
     }
 }
-const db1 = Database.getDatabase('localhost', 'root', '123');
-const db2 = Database.getDatabase('localhost', 'root', '123');
-console.log(db1 === db2);
+class Arqueiro extends Personagem {
+    bordao() {
+        console.log('Por Baldur derrotarei todos os Orcs, AAAAAAAAAAAA');
+    }
+}
+class Orc extends Personagem {
+    bordao() {
+        console.log('Um pequeno verme, matarei lentamente.....');
+    }
+}
+const arqueiro = new Arqueiro('Arqueiro', 100, 1000);
+const orc = new Orc('Orc', 90, 1000);
+arqueiro.atacar(orc);
+arqueiro.atacar(orc);
+arqueiro.atacar(orc);
