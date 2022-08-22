163685d354274118de42b5dcdd3b7813bb212554
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-22 20:27:33 -0300

    Generics Utility types.

--- Ambient/index.js
+++ Ambient/index.js
@@ -1,19 +1,34 @@
 "use strict";
-function isNumber(a) {
-    return typeof a === 'number';
-}
-console.log(isNumber('12'));
-console.log(isNumber(12));
-function Soma(...args) {
-    const retorno = args.reduce((acc, vl) => {
-        if (isNumber(acc) && isNumber(vl)) {
-            const soma = acc + vl;
-            return soma;
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
         }
-        else
-            return acc;
-    }, 0);
-    return retorno;
+    return t;
+};
+//Record cria uma chave para o tipo
+const objeto1 = {
+    name: 'jefferson',
+    sobrenome: 'guirra',
+    idade: 23
+};
+const obj2 = {
+    idade: 23,
+    nome: 'jefferson',
+    sobrenome: 'idade'
+};
+const obj3 = {};
+const accountMongo = {
+    _id: 'ldkcjlkpfçlel',
+    age: 23,
+    name: 'jefferson'
+};
+function mapAccount(AccountMongo) {
+    const { _id } = AccountMongo, AccountData = __rest(AccountMongo, ["_id"]);
+    return Object.assign(Object.assign({}, AccountData), { id: _id });
 }
-console.log(Soma(1, 2, 4));
-console.log(Soma(...[1, 2, 3, 'a', 'b', 'c', 1]));
+console.log(mapAccount(accountMongo));
