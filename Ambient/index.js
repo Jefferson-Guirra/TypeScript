4ee76ec288d16ff2eaa1a43f370e4618d871638b
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-22 21:27:52 -0300

    Decorator

--- Ambient/index.js
+++ Ambient/index.js
@@ -1,34 +1,39 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Animal = void 0;
+let Animal = class Animal {
+    constructor(cor, nome) {
+        this.cor = cor;
+        this.nome = nome;
+    }
+};
+Animal = __decorate([
+    decorator,
+    __metadata("design:paramtypes", [String, String])
+], Animal);
+exports.Animal = Animal;
+function decorator(value) {
+    return class extends value {
+        constructor(...args) {
+            super(...args);
+            this.cor = this.inverte(args[0]);
+            this.nome = this.inverte(args[1]);
         }
-    return t;
-};
-//Record cria uma chave para o tipo
-const objeto1 = {
-    name: 'jefferson',
-    sobrenome: 'guirra',
-    idade: 23
-};
-const obj2 = {
-    idade: 23,
-    nome: 'jefferson',
-    sobrenome: 'idade'
-};
-const obj3 = {};
-const accountMongo = {
-    _id: 'ldkcjlkpfçlel',
-    age: 23,
-    name: 'jefferson'
-};
-function mapAccount(AccountMongo) {
-    const { _id } = AccountMongo, AccountData = __rest(AccountMongo, ["_id"]);
-    return Object.assign(Object.assign({}, AccountData), { id: _id });
+        inverte(valor) {
+            return valor.split('').reverse().join('');
+        }
+    };
 }
-console.log(mapAccount(accountMongo));
+//const animalDecorator = Decorator(Animal)
+const animal = new Animal('azul', 'Gato');
+console.log(animal.cor);
+console.log(animal.nome);
