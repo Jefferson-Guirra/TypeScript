9d3fe778c4a9c3a3bf56524a7ba17da5e660b356
Jefferson Guirra <jeffersonloop14@gmail.com>
2022-08-25 18:30:46 -0300

    updates Ts

--- Ambient/index.js
+++ Ambient/index.js
@@ -1,35 +1,61 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.UmaPessoa = void 0;
-function decorator(classPrototype, nome) {
-    let valorPropriedade;
-    return {
-        get: () => valorPropriedade,
-        set: (valor) => valorPropriedade = valor.split('').reverse().join('')
-    };
-}
-class UmaPessoa {
-    constructor(nome, sobrenome) {
-        this.nome = nome;
-        this.sobrenome = sobrenome;
+exports.ShoppingCart = void 0;
+class ShoppingCart {
+    constructor() {
+        this._items = [];
+        this._orderStatus = 'open';
     }
-    msg() {
-        return `${this.nome} ${this.sobrenome}`;
+    addItem(item) {
+        if (this._orderStatus == 'open')
+            this._items.push(item);
+        else
+            return;
+    }
+    removeItem(index) {
+        this._items.splice(index, 1);
+    }
+    get items() {
+        return this._items;
+    }
+    get orderStatus() {
+        return this._orderStatus;
+    }
+    total() {
+        return +this.items.reduce((acc, valor) => acc + valor.price, 0).toFixed(2);
+    }
+    checkout() {
+        if (this.isEmpty())
+            console.log('seu carrinho está vazio');
+        else {
+            this._orderStatus = 'closed';
+            this.sendMessage(`Seu pedido com total de ${this.total()} está sendo processado.`);
+            this.saveOrder();
+            this.clear();
+        }
+    }
+    isEmpty() {
+        return this._items.length == 0;
+    }
+    sendMessage(value) {
+        console.log(value);
+    }
+    clear() {
+        this._items.length = 0;
+        console.log('carrinho de compras foi limpo.');
+    }
+    saveOrder() {
+        console.log('pedido salvo com sucesso...');
     }
 }
-__decorate([
-    decorator,
-    __metadata("design:type", String)
-], UmaPessoa.prototype, "nome", void 0);
-exports.UmaPessoa = UmaPessoa;
-const pessoa = new UmaPessoa('jefferson', 'guirra');
-console.log(pessoa.msg());
+exports.ShoppingCart = ShoppingCart;
+const shoppingCart = new ShoppingCart();
+console.log(shoppingCart.orderStatus);
+shoppingCart.addItem({ name: 'bolacha', price: 1.5 });
+shoppingCart.addItem({ name: 'caderno', price: 15.50 });
+shoppingCart.addItem({ name: 'camiseta', price: 49.9 });
+console.log(shoppingCart.items);
+console.log(shoppingCart.total());
+shoppingCart.checkout();
+console.log(shoppingCart.items);
+console.log(shoppingCart.orderStatus);
